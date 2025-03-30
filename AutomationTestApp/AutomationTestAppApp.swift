2fd49bd49eed4dd091232787c92457bd89d6a01e
Lior <liorwa052@gmail.com>
2025-03-30 15:21:14 +0300

    Initial Commit

---
+++ AutomationTestApp/AutomationTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationTestAppApp.swift
+//  AutomationTestApp
+//
+//  Created by lior on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutomationTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
